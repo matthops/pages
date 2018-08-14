9099469fc42e103e1910966fe7e04d6040047ccd
Matt Hopkins <matthops@gmail.com>
2018-08-13 20:37:11 -0500

    started setting up server and db

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,18 @@
 const pc = require("./controller");
 const express = require("express");
 const bodyParser = require("body-parser");
+const massive = require("massive");
+const port = process.env.port || 4000;
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(4000, () => {
-  console.log(`server listening on port 4000`);
+massive(process.env.CONNECTION_STRING).then(dbInstance =>
+  app.set("db", dbInstance)
+);
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
